e309a9e3bc09b57a7b6f6708df9459b5f0c49929
aleksandramt <lawrhill.ml@gmail.com>
2020-02-05 11:31:40 +0100

    client page style changes

---
+++ client/src/views/Home/Home.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  #home.home.home-bg-pic( :style="{'background-image': 'url(' + image + ')'}")
+    .home-wrapper
+</template>
+
+<script>
+export default {
+  name: 'home',
+  computed: {
+    image() {
+      // eslint-disable-next-line global-require
+      return require('./../../assets/home-bg.jpg');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  @import './Home';
+</style>
