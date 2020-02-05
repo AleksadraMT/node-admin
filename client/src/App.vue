3302acb853206af1030ebb1118f133ecb210140a
aleksandramt <lawrhill.ml@gmail.com>
2020-02-05 11:31:31 +0100

    client page style changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,19 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app.app
+    Navigation
+    router-view
 </template>
 
+<script>
+export default {
+  name: 'App',
+  components: {
+    Navigation: () => import('./components/Navigation/Navigation'),
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  @import '../node_modules/normalize-scss/sass/_normalize.scss';
+  @import './App.scss';
 </style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../views/Home/Home.vue';
 
 Vue.use(VueRouter);
 
@@ -13,15 +13,23 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About/About'),
+  },
+  {
+    path: '/work',
+    name: 'work',
+    component: () => import('../views/Work/Work'),
+  },
+  {
+    path: '/contacts',
+    name: 'contacts',
+    component: () => import('../views/Contacts/Contacts'),
   },
 ];
 
 const router = new VueRouter({
   mode: 'history',
+  linkActiveClass: 'active',
   base: process.env.BASE_URL,
   routes,
 });
